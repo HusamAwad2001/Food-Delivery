e09f0c500991043bd71f110d537519bdafc2ab22
wwwma <www.hosamawad2016@gmail.com>
2022-09-14 12:24:02 +0300

    finish ui food detail screen

---
+++ lib/widgets/expandable_text_widget.dart
@@ -0,0 +1,8 @@
+class ExandableTextWidget extends StatelessWidget {
+  const ExandableTextWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
